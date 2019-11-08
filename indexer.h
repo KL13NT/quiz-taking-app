fca89f75d971f1fc4885579f9ba3f551478d4d21
Nabil <nabil.tharwat@outlook.com>
2019-11-08 14:34:05 +0200

    DisplayQuestion & DisplayQuestionWithAnswers added to Questions
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- indexer.h
+++ indexer.h
@@ -27,21 +27,26 @@ void MainMenu();
 void QuestionsMenu();
 void QuestionsMenuHandler();
 void AllUsersMenu();
+
 bool CheckCurrentQuestionPoolSize(int, int);
 bool CheckAnswerValidity(const Question &, const string &);
 bool CreateQuestion();
 bool RandomiseAndPrintAnswers(Question);
 bool DeleteQuestion(int);
 bool IsDuplicateQuestion(const Question &, const unordered_set<string> &);
+
 string GetUserInput(string);
 string StringToLowerCase(string);
 string StringToUpperCase(string);
 string IndentString(const string, const int);
 string FormatQuestionTitle(string);
+
 MCQQuestion LoadMCQQuestion(std::ifstream &, string &);
 MCQQuestion CreateMCQQuestion();
+
 CompleteQuestion LoadCompleteQuestion(std::ifstream &, string &);
 CompleteQuestion CreateCompleteQuestion();
+
 TFQuestion LoadTFQuestion(std::ifstream &, string &);
 TFQuestion CreateTFQuestion();
 
